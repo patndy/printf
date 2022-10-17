828e0f9a46033d1fe0b56c7df6627bdee6b676c8
Patrick Ndifreke <patrickndifreke@gmail.com>
2022-10-17 08:40:43 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _print(char c);
+int signed_num( int e);
+int unsigned_num(unsigned int e);
+int _printf(char *fmt,...);
+int octal_num(unsigned int e);
+int hexCap_num(unsigned int e);
+int hexSma_num(unsigned int e);
+#define INT_MAX  2147483647
